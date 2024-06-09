67d8a7f1989fd5427a78f1fcc39f3b2b88746ee6
Adarsh <ashu2100ag@gmail.com>
2024-06-09 22:15:02 +0530

    Made some changes

--- Favorite Movie Guess/Favorite_Movie.js
+++ Favorite Movie Guess/Favorite_Movie.js
@@ -1,15 +1,23 @@
-let favorite = "avator";
+let favorite = ["avator","Avator","Iron Man","iron man","Meg","meg","HarryPotter","Harry Potter","harrypotter","harry potter"];
 let guess = prompt("Guess the movie:");
-while ((guess != favorite) && ((guess != "Quit")||(guess !="quit")))
+h1=document.querySelector("h1");
+if ((guess == "Quit")||(guess=="quit")) {
+    h1.innerText=`You quit the game.....`;
+    console.log("You quit the game......");
+    
+}else{
+while (((!favorite.includes(guess))) && ((guess != "Quit")||(guess !="quit")))
 {
-    guess = prompt("Wrong! Try Again....");
+    guess = prompt("Wrong! Try Again.... or use Quit to quit");
     if ((guess == "Quit")||(guess=="quit")) {
+        h1.innerText=`You quit the game...`;
         console.log("You quit the game......");
         break; 
     }
 }   
-
-if (guess == favorite)
+if (favorite.includes(guess))
 { 
+    h1.innerText=`You guessed it! The favorite movie is indeed ${guess}`;
     console.log("Congo! You guessed it right");
+}
 }
